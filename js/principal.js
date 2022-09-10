a0d28544eb5dea1e0da34da4650355617cbb5071
Erickson Fernandes <ericksound10@gmail.com>
2022-09-10 19:04:20 -0300

    Calculando IMC - Primeiro teste de calculo

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,22 @@
 var titulo = document.querySelector(".titulo"); //Usando a função QuerySelector para selcionar uma classe e alterar o conteúdo da mesma.
 
 titulo.textContent = "Nutricionista";
+
+//Para caucular o IMC foi criado a variavel paciente para extrair a informação do ID 'primeiro-paciente'
+//Foi criado as variaveis de 'peso' e 'altura' com o conteúdo do peso e altura do primeiro paciente.
+//Foi criado a variavel imc para armazenar o calculo de peso /peso / (altura * altura)
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(altura);
+console.log(peso);
+
+var imc = peso / (altura * altura);
+
+console.log(imc);
