fcbd490615acc4b21b26ccc786bab113fec68494
Erickson Fernandes <ericksound10@gmail.com>
2022-09-12 18:44:23 -0300

    Add animação na remoção de pacientes.

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,11 +1,16 @@
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(event){
+tabela.addEventListener("dblclick", function (event) {
 
+    event.target.parentNode.classList.add("transicao-remover");
+
+    setTimeout(function () {
         event.target.parentNode.remove(); // O target identifica quem sofreu o evento(duplo clique), para remover somente quem sobre o evento.
-        console.log(tabela);
 
-    });
+
+    }, 500);
+
+});
 
 
 
