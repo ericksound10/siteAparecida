530f5abd28b95eb94721f1c41d30f8c4007bbe23
Erickson Fernandes <ericksound10@gmail.com>
2023-01-06 00:22:17 -0300

    Capturando erros de requisição

--- js/importar-paciente.js
+++ js/importar-paciente.js
@@ -1,21 +1,24 @@
 var botaoImportar = document.querySelector("#importar-paciente");
 
-botaoImportar.addEventListener("click", function() {
+botaoImportar.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master11/pacientes.json");
 
-    xhr.addEventListener("load", function() {
-        console.log(xhr.responseText);
+    xhr.addEventListener("load", function () {
+        var erroImport = document.querySelector("#erroImport");
+        //Se a requisição for true 
+        if (xhr.status == 200) {
+            erroImport.classList.add("ocultaPaciente");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        var resposta = xhr.responseText;
-
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
-        
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroImport.classList.remove("ocultaPaciente");
+        }
     });
 
     xhr.send();
